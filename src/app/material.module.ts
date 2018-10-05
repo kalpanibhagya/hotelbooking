0e85f574107d6dc2d4d506b6c1090938b4723e8d
Kalpani Ranasinghe <KalpaniR@99x.lk>
2018-10-05 16:00:20 +0530

    Connect navigation component to home

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material';
 import {MatButtonModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+
 
 @NgModule({
     imports: [MatBadgeModule,
-        MatButtonModule],
-    exports: [ MatBadgeModule, MatButtonModule ]
+        MatButtonModule, MatSidenavModule, MatToolbarModule],
+    exports: [ MatBadgeModule, MatButtonModule, MatSidenavModule , MatToolbarModule]
 })
 
 export class MaterialModule { }
